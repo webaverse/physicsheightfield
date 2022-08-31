a9e24ac3a96f8eabfcef42d1554c90a535517869
gonnavis <gonnavis@163.com>
2022-08-31 17:21:56 +0800

    add arg: heights.

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ export default () => {
   const app = useApp();
   const physics = usePhysics();
   
-  const numRows = 10; // x axis
-  const numColumns = 5; // z axis
-  const heightScale = 1;
-  const rowScale = 1; // x axis
-  const columnScale = 1; // z axis
-  physics.addHeightFieldGeometry(numRows, numColumns, heightScale, rowScale, columnScale);
+  const numRows = 10; // int, x axis
+  const numColumns = 5; // int, z axis
+  const heightScale = 1; // float
+  const rowScale = 1; // float, x axis
+  const columnScale = 1; // float, z axis
+  const heights = []; // int16
+  const numVerts = numRows * numColumns;
+  for (let i = 0; i < numVerts; i++) {
+    heights[i] = 1;
+  }
+  physics.addHeightFieldGeometry(numRows, numColumns, heights, heightScale, rowScale, columnScale);
   
   return app;
 };
